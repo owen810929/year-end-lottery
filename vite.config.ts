3722e1ef2842f46b17c9400752b2cfa93a626174
owen810929 <mashiaradyr@gmail.com>
2026-06-26 11:20:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/2027company-year-end-party/",
+  base: "/year-end-lottery/",
   plugins: [react()],
 });
